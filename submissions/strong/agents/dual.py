00be238b688f0700e5bdf7d9d8efedfe90915748
Dmitry Sorokin <dmitrii.sorokin@phystech.edu>
2023-08-09 15:27:37 +0300

    rename

--- submissions/strong/agents/dual.py
+++ submissions/strong/agents/dual.py
@@ -21,19 +21,9 @@ class ObservationFunction():
         if done:
             return None
 
-        m = model.as_pyscipopt()
-        variables = m.getVars(transformed=True)
-        inf = m.infinity()
+        sbf = ec.observation.StrongBranchingScores()
+        return sbf.extract(model, False)
 
-        # extract the upper and lower bounds of all variables
-        lbs = np.asarray([v.getLbLocal() for v in variables])
-        ubs = np.asarray([v.getUbLocal() for v in variables])
-        has_lb = np.asarray([lb > -inf for lb in lbs])
-        has_ub = np.asarray([ub < inf for ub in ubs])
-
-        observation = (has_lb, has_ub, lbs, ubs)
-
-        return observation
 
 
 class Policy():
@@ -49,15 +39,6 @@ class Policy():
         self.rng = np.random.RandomState(seed)
 
     def __call__(self, action_set, observation):
-        has_lb, has_ub, lbs, ubs = observation 
-
-        # safety check: make sure all variables in the action set have lower and upper bounds
-        assert all(has_lb[action_set]) and all(has_ub[action_set])
-
-        # decide on (partial) variable assignments
-        var_ids = action_set
-        var_vals = self.rng.randint(lbs[var_ids], ubs[var_ids]+1)  # random sampling
-
-        action = (var_ids, var_vals)
-
-        return action
+        scores = observation
+        scores = scores[action_set]
+        return action_set[np.argmax(scores)]
